a77fea67dd20a22c73f8fa1710ec0f177792dd74
msxtracool <larissasanntos@hotmail.com>
2025-04-25 20:33:39 +0200

    Lab Solved

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,81 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 >= num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
-function findLongestWord() {}
-
-
-
+function findLongestWord(words) {
+  if (words.length === 0) {
+    return null;
+  }
+  let longestWord = "";
+  for (let i = 0; i < words.length; i++) {
+    const currentWord = words[i];
+    if (currentWord.length > longestWord.length) {
+      longestWord = currentWord;
+    }
+  }
+  return longestWord;
+}
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
-
+function sumNumbers(arrNumbers) {
+  let total = 0;
+  for (let i = 0; i < arrNumbers.length; i++) {
+    const currentNumber = arrNumbers[i];
+    total += currentNumber;
+  }
+  return total;
+}
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
-
-
+function averageNumbers() {
+  let arrNumbers2Elements = 0;
+  for (let i = 0; i < numbers2.length; i++) {
+    arrNumbers2Elements += numbers2[i];
+  }
+  return arrNumbers2Elements;
+}
 
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const words2 = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
-function doesWordExist() {}
+function doesWordExist(words2, matter) {
+  // if array is empty
+  if (words2.length === 0) {
+    return null;
+  }
+
+  if (words2.includes(matter)) {
+    return true;
+  }
+
+  return false;
+}
